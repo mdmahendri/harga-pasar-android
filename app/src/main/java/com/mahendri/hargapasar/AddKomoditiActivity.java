7a78ebb524d6e96836cd220ed36e4e4b24da990f
mahendri.md <mahendri.md@gmail.com>
2018-01-27 20:18:50 +0700

    using Fused Loation Client to request LatLng

--- app/src/main/java/com/mahendri/hargapasar/AddKomoditiActivity.java
+++ app/src/main/java/com/mahendri/hargapasar/AddKomoditiActivity.java
@@ -1,13 +1,11 @@
 package com.mahendri.hargapasar;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,12 +14,25 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class AddKomoditiActivity extends AppCompatActivity
-        implements LocationListener, View.OnClickListener {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
+import java.util.Locale;
+
+public class AddKomoditiActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_PERMISSION_LOCATION = 1;
+
+    private View rootLayout;
     private TextInputEditText locationText;
+    private GoogleApiClient googleApiClient;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
 
-    private LocationManager locationManager;
     private double longitude;
     private double latitude;
 
@@ -36,71 +47,33 @@ public class AddKomoditiActivity extends AppCompatActivity
         }
 
         setContentView(R.layout.activity_add_komoditi);
+        rootLayout = findViewById(R.id.root_layout);
         locationText = findViewById(R.id.text_lat_lng);
         TextView tempatText = findViewById(R.id.text_tempat);
-        tempatText.setText(namaTempat);
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager != null) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-                return;
-            }
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 10, this);
-        }
-
         FloatingActionButton fab = findViewById(R.id.add_komoditi_fab);
+
+        tempatText.setText(namaTempat);
         fab.setOnClickListener(this);
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-            return;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            updateLocation();
+        } else {
+            // belum mendapat permission
+            requestLocationPermission();
         }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 10, this);
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        longitude = location.getLongitude();
-        latitude = location.getLatitude();
-
-        locationText.setText("(" + latitude + "," + longitude + ")");
+    protected void onPause() {
+        super.onPause();
+        fusedLocationClient.removeLocationUpdates(locationCallback);
     }
 
     @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
+    protected void onResume() {
+        super.onResume();
+        updateLocation();
     }
 
     @Override
@@ -112,6 +85,74 @@ public class AddKomoditiActivity extends AppCompatActivity
                 else
                     Toast.makeText(this, "lanjut ke simpan", Toast.LENGTH_SHORT).show();
                 break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+
+            // granted permission
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                updateLocation();
+            } else {
+                // meminta permission lagi
+                requestLocationPermission();
+            }
+        }
+    }
+
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            Snackbar.make(rootLayout, "Dibutuhkan akses lokasi untuk mendata komoditas",
+                    Snackbar.LENGTH_SHORT).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // meminta permission
+                    ActivityCompat.requestPermissions(AddKomoditiActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_PERMISSION_LOCATION);
+                }
+            }).show();
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSION_LOCATION);
+        }
+    }
+
+    private void updateLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(5000); // update setiap lima detik
+
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    for (Location location : locationResult.getLocations()) {
+                        longitude = location.getLongitude();
+                        latitude = location.getLatitude();
+
+                        locationText.setText(String.format(Locale.US,"%s, %s",
+                                latitude, longitude));
+                    }
+                }
+            };
+
+            // init untuk pertama kalinya
+            if (fusedLocationClient == null){
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            }
+
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
         }
     }
 }
